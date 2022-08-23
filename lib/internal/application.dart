6df9292830ca691efa60c84b204e29db95ca94fd
devkayumov <devkayumov@gmail.com>
2022-08-23 11:40:46 +0600

    habr: myapp, commit 1

---
+++ lib/internal/application.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../presentation/home.dart';
+
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
